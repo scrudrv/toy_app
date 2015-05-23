a5593e6045e992506c36bef6df5a2a3a8defb473
scrudrv <scrudrv@gmail.com>
2015-05-23 02:08:06 +0000

    Finish toy app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  def hello
-    render text: "never quit"
-  end
+  # def hello
+  #   render text: "never quit"
+  # end
   
 end
